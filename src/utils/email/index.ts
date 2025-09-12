8858d23788d46eb5b74c7f44c3263e016633e396
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-09-12 16:51:26 +0300

    simple modified send gmail

--- src/utils/email/index.ts
+++ src/utils/email/index.ts
@@ -4,18 +4,18 @@ import nodemailer, { Transporter } from "nodemailer";
 
 export async function sendEmail({ to, subject, html }: ISendEmailOptions): Promise<void> {
     const transport: Transporter = nodemailer.createTransport({
-        // host: process.env.EMAIL_HOST, // لو هتستخدم host
-        service: process.env.EMAIL_SERVICE, // لو هتستخدم service زي "gmail"
-        port: Number(process.env.EMAIL_PORT) || 465, // default Gmail SSL
-        secure: true, // لازم true مع 465
+    
+        service: process.env.EMAIL_SERVICE, 
+        port: Number(process.env.EMAIL_PORT) || 465, 
+        secure: true, 
         auth: {
-            user: process.env.USER_EMAIL, // email بتاعك
-            pass: process.env.PASSWORD_EMAIL, // الباسورد أو app password
+            user: process.env.USER_EMAIL, 
+            pass: process.env.PASSWORD_EMAIL, 
         },
     });
 
     await transport.sendMail({
-        from: `'Social Media' <${process.env.USER_EMAIL}>`, // الأفضل تخليها من الـ env
+        from: `'Social Media' <${process.env.USER_EMAIL}>`, 
         to,
         subject,
         html,
