427f69d787a382d0cc06ef4c2422c17214013d77
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-09-25 13:18:48 +0300

    hash password

--- src/utils/hash/index.ts
+++ src/utils/hash/index.ts
@@ -1,8 +1,7 @@
 import bcrypt from "bcryptjs";
-export const hashPassword= async(password:string)=>{
-    const hashPassword=await bcrypt.hash(password,10);   
-    return hashPassword;
+export const hashPassword = async (password: string) => {
+    return await bcrypt.hash(password, 10);
 }
-export const comparePassword=async(password:string,hashPassword:string)=>{
-    return await bcrypt.compare(password,hashPassword);
+export const comparePassword = async (password: string, hashPassword: string) => {
+    return await bcrypt.compare(password, hashPassword);
 }
