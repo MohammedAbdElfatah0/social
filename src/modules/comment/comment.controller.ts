b7850f9cd3f98170530579934ed393840b37eebb
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-10-14 14:30:42 +0300

    fix:create route post and comment delete

--- src/modules/comment/comment.controller.ts
+++ src/modules/comment/comment.controller.ts
@@ -5,4 +5,5 @@ import { authMiddleware } from "../../middleware";
 const commentRouter = Router({ mergeParams: true });
 commentRouter.post("{/:id}", authMiddleware(), CommentService.create);
 commentRouter.get("/:id", authMiddleware(), CommentService.getSpecific);
+commentRouter.delete("/:id", authMiddleware(), CommentService.deleteComment);
 export { commentRouter };
--- src/modules/post/post.controller.ts
+++ src/modules/post/post.controller.ts
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import PostService from "./post.service";
 import { authMiddleware } from "../../middleware";
-import {commentRouter} from "../comment/comment.controller";
+import { commentRouter } from "../comment/comment.controller";
 
 const postRouter = Router();
-postRouter.use("/:postId/comment",commentRouter)
+postRouter.use("/:postId/comment", commentRouter)
 postRouter.post("/create-post", authMiddleware(), PostService.createPost);
 postRouter.patch("/:id", authMiddleware(), PostService.addReaction);
-postRouter.get("/:id", PostService.getSpecific);
+postRouter.get("/:id", authMiddleware(), PostService.getSpecific);
+postRouter.delete("/:id", authMiddleware(), PostService.deletePost);
 export default postRouter;
