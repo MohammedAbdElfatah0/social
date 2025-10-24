430729b1d2340b969a7fa17fb2df902949c1a45c
Mohammed Mohammed Abd El-Fatah <hamoabdelfatah611@gmail.com>
2025-10-24 15:36:13 +0300

    repository for friend

--- src/DB/models/friend/friend.repository.ts
+++ src/DB/models/friend/friend.repository.ts
@@ -1,9 +1,11 @@
-// import { IFriend } from "../../../utils";
-// import { AbstractRepository } from "../../abstract.repository";
-// import friendModel from "./friend.model";
+import { IFriend } from "../../../utils";
+import { AbstractRepository } from "../../abstract.repository";
+import friendModel from "./friend.model";
 
-// export class FriendRepository extends AbstractRepository<IFriend> {
-//     constructor() {
-//         super(friendModel);
-//     }
-// }
+class FriendRepository extends AbstractRepository<IFriend> {
+    constructor() {
+        super(friendModel);
+    }
+}
+    
+export default new FriendRepository();
